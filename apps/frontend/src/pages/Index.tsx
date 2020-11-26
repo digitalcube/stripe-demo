4107e7d5ef39804d8189485e1defcef90219db36
Hidetaka Okamoto <info@wp-kyoto.net>
2020-11-26 20:32:41 +0900

    add content

--- apps/frontend/src/pages/Index.tsx
+++ apps/frontend/src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import React, {FC, useCallback} from 'react'
-import { Card, Container, Grid, Heading, Text, } from '@digitalcube/galaxy'
+import { Benefits, Card, Container, Grid, Heading, Hero, PageHeader, Solutions, Text, } from '@digitalcube/galaxy'
 import { Button } from 'theme-ui'
 import { StripePrice, StripeProduct, useListProductsHook } from '../hooks/products'
 import { useStripeCheckout } from '../hooks/checkout';
@@ -49,10 +49,28 @@ const Product: FC<{
 export const PageIndex: FC = () => {
     const products = useListProductsHook()
     return (
+        <>
+        <Benefits
+            title="Stripe Billing Demoサイトへようこそ"
+            subtitle="Stripe Billing / Checkout / Customer Portalを体験しましょう！"
+            img={<img src="./assets/Stripe-Hosted-Invoices.jpg" style={{maxWidth: '100%', height: 'auto'}} />}
+            nodes={[{
+                title: "Billingで定期課金をお手軽に",
+                excerpt: 'Stripe Billingを使うことで、簡単かつ拡張しやすい形であなたのサービスに定期課金を組み込むことができます。'
+            }, {
+                title: "Checkoutでシンプル/セキュアな購入フロー",
+                excerpt: 'Stripe CheckoutはStripeがホストする購入ページを利用できる仕組みです。自前で購入処理を実装する必要がなく、より手軽より安全に購入画面を導入できます。'
+            }, {
+                title: "支払い管理はCustomer Portalで",
+                excerpt: "Customer Portalを使うことで、カード情報の更新や支払い履歴の確認・プラン変更などのユーザー操作画面を簡単に提供できます。"
+            }]}
+        />
+        <PageHeader title="テスト商品一覧" subtitle="体験用のデモ商品です。クレジットカードはテスト用の番号「4242 4242 4242 4242」、年月やCVCはお好きな番号を入力してお試しください。"/>
         <Container size="4" sx={{py: 7, px: '5%'}}>
             <Grid columns={[1, null, 2, null, 3]}>
                 {products.map(product => <Product product={product} key={product.id} />)}
             </Grid>
         </Container>
+        </>
     )
 }
